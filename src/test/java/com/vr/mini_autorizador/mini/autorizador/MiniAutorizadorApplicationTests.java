86d0cd614242bedb1db5b6b85153894cb8e3747b
claudemirr <z3KLn1mAZ@0>
2025-07-04 13:03:50 -0300

    criação do projeto

---
+++ src/test/java/com/vr/mini_autorizador/mini/autorizador/MiniAutorizadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vr.mini_autorizador.mini.autorizador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniAutorizadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
